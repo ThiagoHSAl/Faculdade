4973a7a2886ebd761193f5f5811a4cfbc81be3c7
Thiago Henrique Silva de Almeida <thiagohenriquesilva.a@gmail.com>
2024-11-01 22:28:40 -0300

    Create empresa.hpp

---
+++ PDS2/Laboratorios/Gestão de Ônibus/empresa.hpp
@@ -0,0 +1,18 @@
+#ifndef EMPRESA_H
+#define EMPRESA_H
+#include "onibus.hpp"
+#include <iostream>
+#include <vector>
+#include <string>
+
+struct frota{
+int quantidade_onibus;
+std::vector<onibus*> minha_frota;
+
+frota();
+void adicionar_onibus(onibus adicionar);
+onibus* buscar_onibus(std::string placa,char print);
+void imprimir_estado();
+};
+
+#endif
