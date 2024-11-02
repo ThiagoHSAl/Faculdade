465fe06bea3bd8e7e32dd1b7613e8b97a0c501d4
Thiago Henrique Silva de Almeida <thiagohenriquesilva.a@gmail.com>
2024-11-01 22:28:57 -0300

    Create empresa.cpp

---
+++ PDS2/Laboratorios/Gestão de Ônibus/empresa.cpp
@@ -0,0 +1,37 @@
+#include "empresa.hpp"
+
+frota::frota(){//construtor da frota
+    quantidade_onibus=0;
+    minha_frota.resize(20,nullptr);
+}
+
+void frota::adicionar_onibus(onibus adicionar){
+    if(buscar_onibus(adicionar.placa,'N')==nullptr){//adiciona um onibus apenas se ele ainda não existe
+        minha_frota[quantidade_onibus]=new onibus(adicionar.placa,adicionar.capacidade_max);
+        quantidade_onibus++;
+        std::cout<<"novo onibus cadastrado"<<std::endl;
+    }
+    else{
+        std::cout<<"ERRO : onibus repetido"<<std::endl;
+    }
+}
+
+onibus* frota::buscar_onibus(std::string placa,char print){//o cadastro depende de uma busca, mas não deve ser printado, char print serve pra isso
+    for(auto busca : minha_frota){
+        if(busca != nullptr && busca->placa==placa){
+            return busca;
+        }
+    }
+    if(print=='P'){
+         std::cout<<"ERRO : onibus inexistente"<<std::endl;
+    }
+    return nullptr;
+}
+
+void frota::imprimir_estado(){//percorre toda a frota imprimindo as informações
+    for(auto busca : minha_frota){
+       if(busca!=nullptr){
+           busca->imprimir_estado();
+       }  
+    }
+}
