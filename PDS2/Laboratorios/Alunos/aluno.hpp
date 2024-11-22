9112eb3da50aeb1bdebfcd95da801fec1b0c9bae
Thiago Henrique Silva de Almeida <thiagohenriquesilva.a@gmail.com>
2024-11-22 10:28:19 -0300

    Create aluno.hpp

---
+++ PDS2/Laboratorios/Alunos/aluno.hpp
@@ -0,0 +1,33 @@
+#ifndef ALUNO_H
+#define ALUNO_H
+#include <string>
+#include <vector>
+
+class aluno{
+    private:
+    std::string nome;
+    int matricula;
+    std::vector<int> notas;
+
+    public:
+    aluno(std::string nome, int matricula, std::vector<int> notas);
+    aluno() : nome(""), matricula(0), notas() {}
+    void media();
+    void min_max();
+    void imprimir_estado();
+    std::string get_nome();
+};
+
+class escola{
+    private:
+    std::vector<aluno> colegio;
+    
+    public:
+
+    escola();
+    void adicionar_aluno(aluno aluno);
+    void relatorio();
+
+};
+#endif
+
